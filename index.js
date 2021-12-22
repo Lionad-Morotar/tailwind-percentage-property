94fa017aa0e0edbc5c9dfaeeb52eb6cb7f47450f
仿生狮子 <tangnad@qq.com>
2021-12-22 16:21:02 +0800

    feat: min-w，min-h，max-w，max-h

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const plugin = require('tailwindcss/plugin')
 
-const percentageWidth = plugin(({ e, addUtilities }) => {
-  const lista = [...Array(100).keys()]
+const percentageProperty = plugin(({ e, addUtilities }) => {
+  const lista = [...Array(101).keys()]
 
   const newUtilities = lista.map((x) => {
     return {
       [`.${e(`w-${x}%`)}`]: {
         width: `${x}%`,
+      },
+      [`.${e(`min-w-${x}%`)}`]: {
+        'min-width': `${x}%`,
+      },
+      [`.${e(`max-w-${x}%`)}`]: {
+        'max-width': `${x}%`,
+      },
+      [`.${e(`h-${x}%`)}`]: {
+        height: `${x}%`,
+      },
+      [`.${e(`min-h-${x}%`)}`]: {
+        'min-height': `${x}%`,
+      },
+      [`.${e(`max-h-${x}%`)}`]: {
+        'max-height': `${x}%`,
       },
     }
   })
@@ -14,4 +29,4 @@ const percentageWidth = plugin(({ e, addUtilities }) => {
   addUtilities(newUtilities)
 })
 
-module.exports = percentageWidth
+module.exports = percentageProperty
